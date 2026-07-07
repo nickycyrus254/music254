5a16c0a6283d976ad82c079f9b49558423be388f
nickycyrus254 <nickycyrus254@gmail.com>
2026-07-08 01:13:15 +0300

    Update tv_page.dart

--- lib/tv_page.dart
+++ lib/tv_page.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
 
+class TvPage extends StatelessWidget {
+  const TvPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF0B1023),
+      appBar: AppBar(
+        title: const Text("MelodyVerse TV"),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: const Center(
+        child: Text(
+          "📺 MelodyVerse TV\n\nComing Soon",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
