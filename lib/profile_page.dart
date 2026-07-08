e48647967e9ce975ceb7830ecb4594f040f2cab4
nickycyrus254 <nickycyrus254@gmail.com>
2026-07-08 03:22:54 +0300

    Update profile_page.dart

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
 
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Creator Profile"),
+      ),
+      body: const Center(
+        child: Text(
+          "Creator Profile Coming Soon",
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
