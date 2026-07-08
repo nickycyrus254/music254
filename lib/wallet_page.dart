a7f3c55868904a1fd9e252b36683a18518bd50ce
nickycyrus254 <nickycyrus254@gmail.com>
2026-07-08 03:22:28 +0300

    Update wallet_page.dart

--- lib/wallet_page.dart
+++ lib/wallet_page.dart
@@ -1,1 +1,43 @@
+import 'package:flutter/material.dart';
 
+class WalletPage extends StatelessWidget {
+  const WalletPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Melody Wallet"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.account_balance_wallet),
+                title: Text("Wallet Balance"),
+                subtitle: Text("\$0.00"),
+              ),
+            ),
+            SizedBox(height: 20),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.upload),
+                title: Text("Withdraw Earnings"),
+              ),
+            ),
+            SizedBox(height: 10),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.history),
+                title: Text("Transaction History"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
