49004946357dcf3b9923bebd9e4d69340dfa56ec
nickycyrus254 <nickycyrus254@gmail.com>
2026-07-08 16:11:52 +0300

    Update project.dart

--- lib/models/project.dart
+++ lib/models/project.dart
@@ -1,15 +1,43 @@
-class Project {
+import 'package:hive/hive.dart';
+
+part 'project.g.dart';
+
+@HiveType(typeId: 0)
+class Project extends HiveObject {
+  @HiveField(0)
   String id;
+
+  @HiveField(1)
   String title;
+
+  @HiveField(2)
   String artist;
+
+  @HiveField(3)
   String genre;
+
+  @HiveField(4)
   String mood;
+
+  @HiveField(5)
   String lyrics;
+
+  @HiveField(6)
   String beat;
+
+  @HiveField(7)
   String melody;
+
+  @HiveField(8)
   String voicePath;
+
+  @HiveField(9)
   String coverArtPath;
+
+  @HiveField(10)
   DateTime createdAt;
+
+  @HiveField(11)
   DateTime updatedAt;
 
   Project({
