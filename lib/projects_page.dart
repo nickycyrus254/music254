0459240e8cf8372b47cc13d005816de73667c21b
nickycyrus254 <nickycyrus254@gmail.com>
2026-07-08 04:01:40 +0300

    Update projects_page.dart

--- lib/projects_page.dart
+++ lib/projects_page.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+
+import 'models/song.dart';
 import 'services/song_service.dart';
 
-class ProjectsPage extends StatelessWidget {
+class ProjectsPage extends StatefulWidget {
   const ProjectsPage({super.key});
 
   @override
+  State<ProjectsPage> createState() => _ProjectsPageState();
+}
+
+class _ProjectsPageState extends State<ProjectsPage> {
+
+  @override
   Widget build(BuildContext context) {
-    final songs = SongService.getSongs();
+
+    final List<Song> songs = SongService.getSongs();
 
     return Scaffold(
       appBar: AppBar(
@@ -22,6 +31,7 @@ class ProjectsPage extends StatelessWidget {
           : ListView.builder(
               itemCount: songs.length,
               itemBuilder: (context, index) {
+
                 final song = songs[index];
 
                 return Card(
@@ -29,10 +39,56 @@ class ProjectsPage extends StatelessWidget {
                   child: ListTile(
                     leading: const Icon(Icons.music_note),
                     title: Text(song.title),
-                    subtitle: Text(song.genre),
+                    subtitle: Text(
+                      "${song.genre} • ${song.mood}",
+                    ),
                     trailing: const Icon(Icons.arrow_forward_ios),
+
+                    onTap: () {
+
+                      showDialog(
+
+                        context: context,
+
+                        builder: (_) {
+
+                          return AlertDialog(
+
+                            title: Text(song.title),
+
+                            content: SingleChildScrollView(
+
+                              child: Text(song.lyrics),
+
+                            ),
+
+                            actions: [
+
+                              TextButton(
+
+                                onPressed: () {
+
+                                  Navigator.pop(context);
+
+                                },
+
+                                child: const Text("Close"),
+
+                              )
+
+                            ],
+
+                          );
+
+                        },
+
+                      );
+
+                    },
+
                   ),
                 );
+
               },
             ),
     );
