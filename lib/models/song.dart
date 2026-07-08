6c492e835c0b58cd2f33abb0999e968c0ad5afd3
nickycyrus254 <nickycyrus254@gmail.com>
2026-07-08 03:39:50 +0300

    Create Song model with required fields

---
+++ lib/models/song.dart
@@ -0,0 +1,17 @@
+class Song {
+  final String title;
+  final String genre;
+  final String mood;
+  final String artist;
+  final String prompt;
+  final String lyrics;
+
+  Song({
+    required this.title,
+    required this.genre,
+    required this.mood,
+    required this.artist,
+    required this.prompt,
+    required this.lyrics,
+  });
+}
